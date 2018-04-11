b8ee858577f4dd01afc4ca8d6471ee2447dfe167
Dmitry <d.k.kulikov@gmail.com>
2018-04-11 12:34:48 +0300

    Add base music bot functions

--- QuorraWeb/Interfaces/IMusicService.cs
+++ QuorraWeb/Interfaces/IMusicService.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 
 namespace QuorraWeb.Interfaces
 {
-    interface IMusicService
+    public interface IMusicService
     {
+        Task HandleQueryAsync(string query);
     }
 }
